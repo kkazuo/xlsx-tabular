574fddd69223b293886f63516c014ad2f922a2eb
Björn Buckwalter <bjorn@buckwalter.se>
2016-10-18 20:58:03 +0200

    Style changes

--- src/Codec/Xlsx/Util/Tabular.hs
+++ src/Codec/Xlsx/Util/Tabular.hs
@@ -29,25 +29,19 @@ module Codec.Xlsx.Util.Tabular
 import Codec.Xlsx.Util.Tabular.Imports
 import qualified Data.ByteString.Lazy as ByteString
 
-type Rows =
-  [(Int, Cols)]
+type Rows = [(Int, Cols)]
 
-type Cols =
-  [(Int, Cell)]
+type Cols = [(Int, Cell)]
 
-type RowValues =
-  [(Int, [(Int, Maybe CellValue)])]
+type RowValues = [(Int, [(Int, Maybe CellValue)])]
 
 
 -- |Read from Xlsx file as tabular rows
 toTableRowsFromFile :: Int -- ^ Starting row index (header row)
                     -> String -- ^ File name
                     -> IO (Maybe Tabular)
-toTableRowsFromFile offset fname = do
-  s <- ByteString.readFile fname
-  let xlsx = toXlsx s
-      rows = toTableRows' xlsx offset
-  pure rows
+toTableRowsFromFile offset fname =
+  flip toTableRows' offset . toXlsx <$> ByteString.readFile fname
 
 -- |Decode cells as tabular rows.
 toTableRows :: Xlsx -- ^ Xlsx Workbook
@@ -58,23 +52,15 @@ toTableRows xlsx sheetName offset =
   decodeRows <$> styles <*> Just offset <*> rows
   where
     styles = parseStyleSheet (xlsx ^. xlStyles) ^? _Right
-    rows =
-      xlsx
-      ^? ixSheet sheetName
-      . wsCells
-      . to toRows
+    rows = xlsx ^? ixSheet sheetName . wsCells . to toRows
 
 -- |Decode cells as tabular rows from first sheet.
 toTableRows' :: Xlsx -- ^ Xlsx Workbook
              -> Int -- ^ Starting row index (header row)
              -> Maybe Tabular
-toTableRows' xlsx offset =
-  toTableRows xlsx firstSheetName offset
+toTableRows' xlsx = toTableRows xlsx firstSheetName
   where
-    firstSheetName =
-      xlsx ^. xlSheets
-      & head
-      & fst
+    firstSheetName = fst $ head $ xlsx ^. xlSheets
 
 decodeRows ss offset rs =
   def
@@ -83,102 +69,59 @@ decodeRows ss offset rs =
   where
     rs' = getCells ss offset rs
     header = head rs' ^. _2
-    header' =
-      header
-      & fmap toText
-      & join
+    header' = join $ toText <$> header
     toText (i, Just (CellText t)) = [def
                                      & tabularHeadIx .~ i
                                      & tabularHeadLabel .~ t]
     toText _ = []
-    cix = fmap (view tabularHeadIx) header'
-      & fromList
-    rows =
-      fmap rowValue (tail rs')
-    rowValue rvs =
-      def
-      & tabularRowIx .~ (rvs ^. _1)
-      & tabularRowCells .~ (rvs ^. _2 & fmap f & join)
+    cix = fmap (view tabularHeadIx) header' & fromList
+    rows = fmap rowValue (tail rs')
+    rowValue rvs = def
+                 & tabularRowIx .~ (rvs ^. _1)
+                 & tabularRowCells .~ (rvs ^. _2 & fmap f & join)
       where
-        f (i, cell) =
-          [cell | cix ^. contains i]
+        f (i, cell) = [cell | cix ^. contains i]
 
 -- |行から値のあるセルを取り出す
 getCells :: StyleSheet -- ^スタイルシート
          -> Int -- ^開始行
          -> Rows -- ^セル行
          -> RowValues
-getCells ss i rs =
-  startAt ss i rs
-  & takeContiguous i
-  & takeUntil ss
-  & fmap rvs
-  & filter vs
-  where
-    rvs (i, cs) =
-      (i, rowValues cs)
-    filter =
-      Prelude.filter
-    vs (i, cs) =
-      any (\(_, v) -> isJust v) cs
+getCells ss i = filter (any (isJust . snd) . snd)
+              . (fmap . fmap) rowValues
+              . takeContiguous i
+              . takeUntil ss
+              . startAt ss i
 
 startAt :: StyleSheet -> Int -> Rows -> Rows
-startAt ss i rs =
-  dropWhile f rs
-  where
-    f (x, _) =
-      x < i
+startAt ss i = dropWhile ((< i) . fst)
 
 -- |指定の行から連続している行を取り出す
 takeContiguous :: Int -> Rows -> Rows
-takeContiguous i rs =
-  [r | (x, r@(y, _)) <- zip [i..] rs, x == y]
+takeContiguous i rs = [r | (x, r@(y, _)) <- zip [i..] rs, x == y]
 
 -- |有効セルのすべてに枠線（Bottom側）が存在しなくなる
 -- |すなわち枠囲みの欄外になるまでの行を取り出す
 takeUntil :: StyleSheet -> Rows -> Rows
-takeUntil ss rs =
-  takeWhile f rs
+takeUntil ss = takeWhile f
   where
-    f (i, cs) =
-      or $ rowBordersHas borderBottom ss cs
+    f (i, cs) = or $ rowBordersHas borderBottom ss cs
 
-rowBordersHas v ss cs =
-  x
-  where
-    x =
-      fmap f cs
-    f (i, cell) =
-      cellHasBorder ss cell v
+rowBordersHas v ss = fmap (cellHasBorder v ss . snd)
 
-rowValues cs =
-  x
-  where
-    x =
-      fmap f cs
-    f (i, cell) =
-      (i, cell ^. cellValue)
+rowValues = fmap (fmap (view cellValue))
 
-cellHasBorder ss cell v =
-  fromMaybe False mb
+cellHasBorder v ss cell = fromMaybe False mb
   where
-    b = cellBorder ss cell
-    mb = borderStyleHasLine v <$> b
+    mb = borderStyleHasLine v <$> cellBorder ss cell
 
 cellBorder :: StyleSheet -> Cell -> Maybe Border
-cellBorder ss cell =
-  view cellStyle cell
-  >>= pure . xf
-  >>= view cellXfBorderId
-  >>= pure . bd
+cellBorder ss cell = fmap xf (view cellStyle cell)
+                 >>= fmap bd . view cellXfBorderId
   where
     xf n = (ss ^. styleSheetCellXfs) !! n
     bd n = (ss ^. styleSheetBorders) !! n
 
-borderStyleHasLine v b =
-  fromMaybe False value
+borderStyleHasLine v b = fromMaybe False value
   where
-  value =
-    view v b
-    >>= view borderStyleLine
-    >>= pure . (/= LineStyleNone)
+    value = view v b >>= fmap (/= LineStyleNone) . view borderStyleLine
